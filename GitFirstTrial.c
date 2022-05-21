9842bba286bf58a3861e7bc40822d9a8e400d694
sucheta-123 <sucheta.dudwadkar@gmail.com>
2022-05-21 16:46:36 +0530

    Create GitFirstTrial.c

---
+++ GitFirstTrial.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {   
+    int number;
+   
+    printf("Enter an integer : ");  
+    
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("FILE FROM GITHUB You entered: %d", number);
+    
+    return 0;
+}
